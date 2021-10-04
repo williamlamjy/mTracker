7dd9046d88e48521f2cc92ad0338dc0b08f7ba78
theodorekwok <theodorekwok@gmail.com>
2021-10-04 14:50:13 +0800

    Add instrument files

---
+++ src/main/java/seedu/mtracker/instrument/subinstrument/Crypto.java
@@ -0,0 +1,6 @@
+package seedu.mtracker.instrument.subinstrument;
+
+import seedu.mtracker.instrument.Instrument;
+
+public class Crypto extends Instrument {
+}
---
+++ src/main/java/seedu/mtracker/instrument/subinstrument/Etf.java
@@ -0,0 +1,6 @@
+package seedu.mtracker.instrument.subinstrument;
+
+import seedu.mtracker.instrument.Instrument;
+
+public class Etf extends Instrument {
+}
