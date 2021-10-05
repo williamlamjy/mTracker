6acf04767e3cafb778ef5672169c7085d66b8a57
theodorekwok <theodorekwok@gmail.com>
2021-10-05 10:12:09 +0800

    Update TextUi to display user facing instructions for adding instrument

--- src/main/java/seedu/mtracker/ui/TextUi.java
+++ src/main/java/seedu/mtracker/ui/TextUi.java
@@ -1,4 +1,24 @@
 package seedu.mtracker.ui;
 
 public class TextUi {
+
+    public static void displayAddInstrumentInstructionOne() {
+        System.out.println("Please key in the type of instrument: ");
+    }
+
+    public static void displayAddInstrumentNameInstruction(String instrumentType) {
+        System.out.println("Name of " + instrumentType + ": ");
+    }
+
+    public static void displayAddInstrumentCurrentPriceInstruction() {
+        System.out.println("Current Price: ");
+    }
+
+    public static void displayAddInstrumentSentimentInstruction() {
+        System.out.println("Sentiment of security: ");
+    }
+
+    public static void displayAddRemarksInstruction() {
+        System.out.println("Remarks (optional): ");
+    }
 }
