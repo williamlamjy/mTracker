3a7eba8f48ff35b2772ec0e4dca5111416ee1ebb
theodorekwok <theodorekwok@gmail.com>
2021-10-07 18:54:18 +0800

    Add new command to handle invalid instrument

---
+++ src/main/java/seedu/mtracker/commands/AddHelpCommand.java
@@ -0,0 +1,9 @@
+package seedu.mtracker.commands;
+
+public class AddHelpCommand extends AddInstrumentCommand {
+
+    @Override
+    public String execute() {
+        return COMMAND_WORD;
+    }
+}
