fa0ceb4e252f5629c024ae513901ac5bf0c757b6
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-10-09 22:16:06 +0800

    Rough commit

--- src/main/java/seedu/mtracker/MTracker.java
+++ src/main/java/seedu/mtracker/MTracker.java
@@ -1,27 +1,42 @@
 package seedu.mtracker;
 
+import seedu.mtracker.commands.Command;
+import seedu.mtracker.commands.ExitCommand;
+import seedu.mtracker.console.InputParser;
 import seedu.mtracker.instrument.InstrumentManager;
+import seedu.mtracker.ui.TextUi;
 
 import java.util.Scanner;
 
 public class MTracker {
 
-    private InstrumentManager instrumentManager = InstrumentManager.getInstance();
+    private InstrumentManager instrumentManager;
+
+    public MTracker() {
+        instrumentManager = InstrumentManager.getInstance();
+    }
+
+    public void run() {
+//        TextUi.greetAtStartUp();
+
+        String userInput = InputParser.getUserInput();
+        String[] inputComponents = InputParser.getCommandComponents(userInput);
+        try {
+            Command c = InputParser.filterByCommandType(inputComponents);
+            while (!c.execute().equals(ExitCommand.COMMAND_WORD)) {
+                c.execute();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Main entry-point for the mTracker application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
+        new MTracker().run();
         //command.setData(instrumentManager, ui); uncomment after initialising ui and command
-        System.out.println("Hello " + in.nextLine());
     }
+
 }
