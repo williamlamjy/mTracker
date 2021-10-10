38ecdbe140c219daf81f7d3f05c67def19e80a3a
williamlamjy <e0543903@u.nus.edu>
2021-10-10 19:30:17 +0800

    Edited test cases

--- src/test/java/seedu/mtracker/console/AddCryptoParserTest.java
+++ src/test/java/seedu/mtracker/console/AddCryptoParserTest.java
@@ -20,8 +20,8 @@ class AddCryptoParserTest {
     public static String USER_INPUT_NO_REMARKS = "TestName%1$s23.4%1$spositive%1$s18Oct%1$s ";
     public static String[] EXPECTED_PARAMS_NO_REMARKS = { "TestName", "23.4", "positive", "18Oct", "" };
 
-    public static String USER_INPUT_WITH_REMARKS = "TestName%1$s100.4%1$snegative%1$s18Oct%1$sTestRemarks";
-    public static String[] EXPECTED_PARAMS_WITH_REMARKS = { "TestName", "100.4", "negative", "18Oct", "TestRemarks" };
+    public static String USER_INPUT_WITH_REMARKS_AND_EXPIRY = "TestName%1$s100.4%1$snegative%1$s18Oct%1$sTestRemarks";
+    public static String[] EXPECTED_PARAMS_WITH_REMARKS_AND_EXPIRY = { "TestName", "100.4", "negative", "18Oct", "TestRemarks" };
 
     public static String USER_INPUT_TRY_INVALID_NAME = "%1$s%1$s%1$sTestName%1$s23.4%1$spositive%1$s18Oct%1$s ";
     public static String USER_INPUT_TRY_INVALID_PRICE = "%1$sTestName%1$s2sd3.4%1$s100.4"
@@ -55,13 +55,18 @@ class AddCryptoParserTest {
     }
 
     @Test
-    void addCryptoParams_allValidParameters_expectSuccess() {
+    void addCryptoParams_noRemarks_expectSuccess() {
         testCryptoParameters(USER_INPUT_NO_REMARKS, EXPECTED_PARAMS_NO_REMARKS);
     }
 
     @Test
-    void addCryptoParams_allValidParametersWithRemarks_expectSuccess() {
-        testCryptoParameters(USER_INPUT_WITH_REMARKS, EXPECTED_PARAMS_WITH_REMARKS);
+    void addCryptoParams_noExpiry_expectSuccess() {
+        testCryptoParameters(USER_INPUT_NO_EXPIRY, EXPECTED_PARAMS_NO_EXPIRY);
+    }
+
+    @Test
+    void addCryptoParams_allValidParametersWithRemarksAndExpiry_expectSuccess() {
+        testCryptoParameters(USER_INPUT_WITH_REMARKS_AND_EXPIRY, EXPECTED_PARAMS_WITH_REMARKS_AND_EXPIRY);
     }
 
     @Test
@@ -71,68 +76,12 @@ class AddCryptoParserTest {
 
     @Test
     void addCryptoParams_tryInvalidPriceMultipleTimes_expectSuccess() {
-        testCryptoParameters(USER_INPUT_TRY_INVALID_PRICE, EXPECTED_PARAMS_WITH_REMARKS);
+        testCryptoParameters(USER_INPUT_TRY_INVALID_PRICE, EXPECTED_PARAMS_WITH_REMARKS_AND_EXPIRY);
     }
 
     @Test
     void addCryptoParams_tryInvalidSentimentMultipleTimes_expectSuccess() {
-        testCryptoParameters(USER_INPUT_TRY_INVALID_SENTIMENT, EXPECTED_PARAMS_WITH_REMARKS);
+        testCryptoParameters(USER_INPUT_TRY_INVALID_SENTIMENT, EXPECTED_PARAMS_WITH_REMARKS_AND_EXPIRY);
     }
-}
-/*
-    public static String emptyInput = "";
-    public static String validRemark = "volatile prices";
-    public static String validExpiry = "sell at 21st October";
-
-    @Test
-    void addCryptoExpiry_validExpiry_expectSuccess() {
-        assertTrue(AddCryptoParser.isValidSpecificParameter(validExpiry));
-    }
-
-    @Test
-    void addCryptoExpiry_emptyInput_expectFailure() {
-        assertFalse(AddCryptoParser.isValidSpecificParameter(emptyInput));
-    }
-
-    @Test
-    void addCryptoRemarks_validRemark_expectSuccess() {
-        assertTrue(AddCryptoParser.isValidSpecificParameter(validRemark));
-    }
-
-    @Test
-    void addCryptoRemarks_emptyInput_expectFailure() {
-        assertFalse(AddCryptoParser.isValidSpecificParameter(emptyInput));
-    }
-
-    @Test
-    void getInstrumentParameters() {
-        // this one is the user input where each input line is separated by %s, notice the space at the end for empty remark
-        String input = String.format("TestName%s23.4%spositive%s18 Oct%snil%s ",
-                System.lineSeparator(),
-                System.lineSeparator(),
-                System.lineSeparator(),
-                System.lineSeparator(),
-                System.lineSeparator());
-
-        // these 2 lines below simulates the console input
-        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
-        System.setIn(bais);
-
-        // these 3 lines gets the console output if any like the "Sorry name must be provided those error messages"
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(baos);
-        System.setOut(printStream);
-
-        // run the method
-        AddCryptoParser testCryptoParser = new AddCryptoParser();
-        testCryptoParser.getInstrumentParameters();
-
-        // below is where you see output so feel free to use below to simulate like empty names, putting empty names multiple times
-        // or check that parameters are recorded exactly
-        String []lines = baos.toString().split(System.lineSeparator());
-        System.out.println(lines);
-        System.out.println(testCryptoParser.getParameters());
-    }
-*/
 
 }
