145e294b17a41429e84b8aeaf7aa4e59680ddc76
theodorekwok <theodorekwok@gmail.com>
2021-10-18 10:46:25 +0800

    Make variable names constant in inputparsertest

--- src/test/java/seedu/mtracker/console/InputParserTest.java
+++ src/test/java/seedu/mtracker/console/InputParserTest.java
@@ -10,52 +10,52 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class InputParserTest {
 
-    public static final String[] noIndexProvidedCommandComponents = { "delete" };
-    public static final String[] invalidIndexProvidedCommandComponents = { "delete", "23r2fc" };
-    public static final String[] validIndexProvidedCommandComponents = { "delete", "4" };
+    public static final String[] NO_INDEX_INPUT = { "delete" };
+    public static final String[] INVALID_INDEX_INPUT = { "delete", "23r2fc" };
+    public static final String[] VALID_INDEX_INPUT = { "delete", "4" };
 
-    public static int INDEX_OFFSET = 1;
-    public static int validIndex = 4;
+    public static final int INDEX_OFFSET = 1;
+    public static final int VALID_INDEX = 4;
 
     @Test
     void getIndexNumber_validIndexProvided_expectSuccess() {
         InputParser parser = new InputParser();
-        parser.getIndexNumber(validIndexProvidedCommandComponents);
-        assertEquals(parser.getInstrumentNumber(), validIndex - INDEX_OFFSET);
+        parser.getIndexNumber(VALID_INDEX_INPUT);
+        assertEquals(parser.getInstrumentNumber(), VALID_INDEX - INDEX_OFFSET);
     }
 
     @Test
     void getIndexNumber_noIndexProvided_expectException() throws InvalidNoIndexError {
         InputParser parser = new InputParser();
         assertThrows(InvalidNoIndexError.class,
-                () -> parser.getIndexNumber(noIndexProvidedCommandComponents));
+                () -> parser.getIndexNumber(NO_INDEX_INPUT));
     }
 
     @Test
     void getIndexNumber_invalidIndexProvided_expectException() throws InvalidIndexError {
         InputParser parser = new InputParser();
         assertThrows(InvalidIndexError.class,
-                () -> parser.getIndexNumber(invalidIndexProvidedCommandComponents));
+                () -> parser.getIndexNumber(INVALID_INDEX_INPUT));
     }
 
     @Test
     void getDeleteInstrumentCommand_noIndexProvided_expectException() throws InvalidNoIndexError {
         InputParser parser = new InputParser();
         assertThrows(InvalidNoIndexError.class,
-                () -> parser.getDeleteInstrumentCommand(noIndexProvidedCommandComponents));
+                () -> parser.getDeleteInstrumentCommand(NO_INDEX_INPUT));
     }
 
     @Test
     void getDeleteInstrumentCommand_invalidIndexProvided_expectException() throws InvalidIndexError {
         InputParser parser = new InputParser();
         assertThrows(InvalidIndexError.class,
-                () -> parser.getDeleteInstrumentCommand(invalidIndexProvidedCommandComponents));
+                () -> parser.getDeleteInstrumentCommand(INVALID_INDEX_INPUT));
     }
 
     @Test
     void getDeleteInstrumentCommand_validIndexProvided_expectSuccess() {
         InputParser parser = new InputParser();
-        DeleteCommand command = parser.getDeleteInstrumentCommand(validIndexProvidedCommandComponents);
-        assertEquals(command.getIndex(), validIndex - INDEX_OFFSET);
+        DeleteCommand command = parser.getDeleteInstrumentCommand(VALID_INDEX_INPUT);
+        assertEquals(command.getIndex(), VALID_INDEX - INDEX_OFFSET);
     }
 }
