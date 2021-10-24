ea0fb10d9eb8ed6b485d08a85f5f11cdf1d37ac4
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-10-24 22:35:31 +0800

    Switch to normal smiley faces

--- src/main/java/seedu/mtracker/ui/TextUi.java
+++ src/main/java/seedu/mtracker/ui/TextUi.java
@@ -121,7 +121,7 @@ public class TextUi {
     public static void displayExitMessage() {
         System.out.println(BYE_LOGO);
         System.out.println("Thank you for using mTracker.\n"
-                + "☻ MAY THE MARKETS BE WITH YOU!!! ᕦ(ò_óˇ)ᕤ");
+                + "☺ ☺ ☺ MAY THE MARKETS BE WITH YOU!!! ☺ ☺ ☺");
     }
 
     public static void displayPrompter() {
