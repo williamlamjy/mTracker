4c5f46e39aa5d8adcebbb9f560d7054d32973f4f
Vignesh Kumaravel <k_vignesh@hotmail.com>
2021-10-30 20:35:28 +0800

    Extend DoneCommand from IndexedCommand

--- src/main/java/seedu/mtracker/commands/DoneCommand.java
+++ src/main/java/seedu/mtracker/commands/DoneCommand.java
@@ -4,28 +4,16 @@ import seedu.mtracker.asserthelpers.AssertCommandHelpers;
 import seedu.mtracker.model.Instrument;
 import seedu.mtracker.ui.TextUi;
 
-public class DoneCommand extends Command {
-
-    public static final int UNINITIALISED_INDEX = -1;
+public class DoneCommand extends IndexedCommand {
     public static final String COMMAND_WORD = "done";
-    private int index;
 
     public DoneCommand() {
         index = UNINITIALISED_INDEX;
     }
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
     @Override
     public String execute() {
-        AssertCommandHelpers.assertIndexWithinBounds(instrumentManager.getSize(), index);
-        Instrument instrumentToComplete = instrumentManager.getInstrument(index);
+        Instrument instrumentToComplete = getInstrumentAtIndex();
         instrumentManager.doneInstrument(index);
         TextUi.displayDoneInstrument(instrumentToComplete);
         return COMMAND_WORD;
