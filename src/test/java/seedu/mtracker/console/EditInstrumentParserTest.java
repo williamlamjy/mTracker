138fbed00fe8efcb5cd37bc5ea9ff65f3ab29f17
wingho2 <e0543725@u.nus.edu>
2021-11-02 18:17:21 +0800

    Fix checkstyle

--- src/test/java/seedu/mtracker/console/EditInstrumentParserTest.java
+++ src/test/java/seedu/mtracker/console/EditInstrumentParserTest.java
@@ -43,7 +43,7 @@ public class EditInstrumentParserTest extends GeneralInstrumentParserTest {
 
     HashMap<String,String> initialiseTestResources(String[] expectedKeys, String[] expectedValues) {
         HashMap<String, String> expectedResult = new HashMap<>();
-        assert(expectedKeys.length == expectedValues.length);
+        assert (expectedKeys.length == expectedValues.length);
         for (int i = 0; i < expectedKeys.length; i++) {
             expectedResult.put(expectedKeys[i], expectedValues[i]);
         }
