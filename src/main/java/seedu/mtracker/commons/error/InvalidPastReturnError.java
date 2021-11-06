2cf984bcc1612494cd165d8c38b22576d11baf0e
theodorekwok <theodorekwok@gmail.com>
2021-11-06 16:26:52 +0800

    Remove irrelevant error files

--- src/main/java/seedu/mtracker/commons/error/InvalidPastReturnError.java
+++
@@ -1,9 +0,0 @@
-package seedu.mtracker.commons.error;
-
-//@@author theodorekwok
-public class InvalidPastReturnError extends Exception {
-    @Override
-    public String getMessage() {
-        return ErrorMessage.INVALID_PAST_RETURN_ERROR;
-    }
-}
--- src/main/java/seedu/mtracker/commons/error/InvalidPastReturnTypeError.java
+++
@@ -1,9 +0,0 @@
-package seedu.mtracker.commons.error;
-
-//@@author theodorekwok
-public class InvalidPastReturnTypeError extends Exception {
-    @Override
-    public String getMessage() {
-        return ErrorMessage.INVALID_PAST_RETURN_TYPE_ERROR;
-    }
-}
